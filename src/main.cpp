4a816d5e21424d143060b3af37c2a5b3227f44b3
Alexander Movsesyan <amovsesy@umd.edu>
2023-04-19 23:57:34 -0400

    compiling wiht antlr

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include "front_end/AluminumLexer.h"
-#include "front_end/AluminumParser.h"
-#include "antlr4-common.h"
+#include <antlr4-common.h>
+#include <fstream>
+#include "ANTLRInputStream.h"
+#include "AluminumLexer.h"
+#include "AluminumParser.h"
 
 int main() {
     std::ifstream stream;
     stream.open("sample_code/hello_world.al");
     antlr4::ANTLRInputStream input(stream);
 //
-//    front_end::AluminumLexer lexer(&input);
-//    antlr4::CommonTokenStream tokens(&lexer);
-//    front_end::AluminumParser parser(&tokens);
+    Aluminum::AluminumLexer lexer(&input);
+    antlr4::CommonTokenStream tokens(&lexer);
+    Aluminum::AluminumParser parser(&tokens);
 
     std::cout << "Hello, World!" << std::endl;
     return 0;
